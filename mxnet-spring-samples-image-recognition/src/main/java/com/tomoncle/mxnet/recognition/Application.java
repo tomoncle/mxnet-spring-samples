707ab3ecfd8caf88b67690af32b7a288f201d36b
tomoncle <1123431949@qq.com>
2019-06-05 19:46:21 +0800

    add idea ide impl files ignore

--- mxnet-spring-samples-image-recognition/src/main/java/com/tomoncle/mxnet/recognition/Application.java
+++ mxnet-spring-samples-image-recognition/src/main/java/com/tomoncle/mxnet/recognition/Application.java
@@ -1,7 +1,6 @@
 package com.tomoncle.mxnet.recognition;
 
 
-import com.tomoncle.config.springboot.EnableSpringBootConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -17,16 +16,10 @@ import org.springframework.context.ApplicationContext;
  */
 
 
-@SpringBootApplication(
-        scanBasePackages = {Application.SCAN_PROJECT_PACKAGE},
-        scanBasePackageClasses = {EnableSpringBootConfig.class}
-)
+@SpringBootApplication(scanBasePackages = {"com.tomoncle.mxnet.recognition"})
 public class Application extends SpringBootServletInitializer {
 
-    static final String SCAN_PROJECT_PACKAGE = "com.tomoncle.mxnet.recognition";
-
     private static Logger logger = LoggerFactory.getLogger(Application.class);
-
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
